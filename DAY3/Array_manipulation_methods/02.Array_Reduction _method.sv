115be66f2b4d7fc6242bd59fcda2d09f457d0e7b
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-12 19:38:25 +0530

    Create 02.Array_Reduction _method.sv

---
+++ DAY3/Array_manipulation_methods/02.Array_Reduction _method.sv
@@ -0,0 +1,18 @@
+module red;
+  int A[]='{5,3,6,7,8};
+  int Res;
+  initial begin
+   
+    Res=A.sum;
+    $display(Res);
+    Res=A.product;
+    $display(Res);
+    Res=A.and;
+    $display(Res);
+    Res=A.or;
+    $display(Res);
+    Res=A.xor;
+    $display(Res);
+    
+  end
+endmodule
