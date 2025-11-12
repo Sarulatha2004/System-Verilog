16cb4db9bb44fa19b6321c717b08b1c21214dc23
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-12 19:57:10 +0530

    Update 02.Struct_with_typedef.sv

--- DAY4/STRUCTURE/02.Struct_with_typedef.sv
+++ DAY4/STRUCTURE/02.Struct_with_typedef.sv
@@ -1,3 +1,24 @@
-
-
-rrgerg
+module struct_with_typedef;
+  typedef struct{
+    string name;
+    bit[63:0]salary;
+    integer id;
+  }employee;
+  
+  initial begin
+    employee emp1,emp2;
+    emp1.name ="Saru";
+    emp1.salary='d250000;
+    emp1.id='d41;
+    $display("employee emp1:%p",emp1);
+    $display("--------------------");
+    
+   
+    emp2.name ="Dd";
+    emp2.salary='d250000;
+    emp2.id='d12;
+    $display("employee emp2:%p",emp2);
+    $display("--------------------");
+    
+  end 
+endmodule
