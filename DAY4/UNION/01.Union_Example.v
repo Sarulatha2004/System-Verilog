32eaad555fd214d7968364865b439ff25b39713e
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-14 15:30:24 +0530

    Update 01.Union_Example.v

--- DAY4/UNION/01.Union_Example.v
+++ DAY4/UNION/01.Union_Example.v
@@ -6,18 +6,13 @@ module union_example;
   } employee;
     
   initial begin
-    employee emp1,emp2;
-   
-    emp1.salary = 'h800;
-    $display("salary updated for EMP1: %h", emp1.salary);
-    emp1.id     = 'd1234;
-    $display("ID updated for EMP: %d", emp1.id);
   
-    
-    emp2.salary = 'h700;
-    $display("salary updated for EMP2: %h", emp2.salary);
-    emp2.id     = 'd4321;
-    $display("ID updated for EMP: %d", emp2.id);
+   
+    employee.salary = 'h800;
+    $display("salary updated for EMP: %h", employee.salary);
+    employee.id     = 'd1234;
+    $display("ID updated for EMP: %d", employee.id);
+
     
     
   end
