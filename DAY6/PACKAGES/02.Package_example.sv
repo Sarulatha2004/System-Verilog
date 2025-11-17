b2cf6ebc72b2a93e2964add20eb95118e043835a
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-17 14:19:02 +0530

    Create 02.Package_example.sv

---
+++ DAY6/PACKAGES/02.Package_example.sv
@@ -0,0 +1,24 @@
+package pkg_A;
+int data =5;
+function pkg_funct();
+  $display("pkg_A:Inside pkg_funct,data=%0d",data);
+endfunction
+endpackage
+
+package pkg_B;
+int data=10;
+function pkg_funct();
+  $display("pkg_B:Inside pkg_funct,data=%0d",data);
+endfunction
+endpackage
+
+import pkg_A::*;
+import pkg_B::*;
+module package_example;
+  initial begin
+    pkg_A::pkg_funct();
+    pkg_B::pkg_funct();
+  end
+endmodule
+           
+  
