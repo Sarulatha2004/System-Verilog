503d06daa917f5a19a5acaac2235a65e8616c800
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-17 14:31:11 +0530

    Update 02.Package_example.sv

--- DAY6/PACKAGES/02.Package_example.sv
+++ DAY6/PACKAGES/02.Package_example.sv
@@ -16,8 +16,8 @@ import pkg_A::*;
 import pkg_B::*;
 module package_example;
   initial begin
-    pkg_A::pkg_funct();
-    pkg_B::pkg_funct();
+    pkg_funct();
+    pkg_funct();
   end
 endmodule
            
