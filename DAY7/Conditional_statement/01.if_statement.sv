bde60a5317612f0af1a2ed03e4acbac286864492
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-18 12:16:23 +0530

    Create 01.if_statement.sv

---
+++ DAY7/Conditional_statement/01.if_statement.sv
@@ -0,0 +1,16 @@
+module if_example;
+  initial begin
+    int a,b;
+    repeat(5)begin
+    a=$urandom_range(1,30);
+    b=$urandom_range(1,30);
+    $display("a=%0d |b=%0d",a,b);
+    if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else
+      $display("a is equal to b");
+  end
+  end
+endmodule
