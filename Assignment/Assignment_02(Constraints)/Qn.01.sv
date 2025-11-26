ee5b69be8b2a56d1a4bf4788164567cf6ced687e
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-11-26 22:18:16 +0530

    Create Qn.01.sv

---
+++ Assignment/Assignment_02(Constraints)/Qn.01.sv
@@ -0,0 +1,36 @@
+class ones_12;
+  rand bit [31:0] data;
+
+  
+  constraint non_consecutive {
+    $countones(data) == 12;
+
+    
+    foreach (data[i])
+      if (i < 31)
+        data[i] + data[i+1] != 2;   
+  }
+
+  function void display();
+    $display("Generated data = %b  ||ones = %0d", data, $countones(data));
+  endfunction
+endclass
+
+module tb;
+  ones_12 ones;
+
+  initial begin
+    ones = new();
+
+    repeat(15) begin
+      if (!ones.randomize())
+        $display("Randomization failed!");
+      else begin
+        ones.display();
+      end
+    end
+
+    $finish;
+  end
+endmodule
+
