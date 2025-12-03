eb0e7db172944022864369d346cde90ab56d57c3
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-03 15:18:50 +0530

    Create Qn_08.sv

---
+++ Assignment/Assignment_03(Array)/Qn_08.sv
@@ -0,0 +1,19 @@
+module array;
+  bit [11:0]my_array[4];
+  initial begin
+    my_array[0]=12'h012;
+    my_array[1]=12'h345;
+    my_array[2]=12'h678;
+    my_array[3]=12'h9AB;
+    $display("Using for loop----->");
+    for(int i=0;i<4;i++)
+      begin
+        $display("my_array[%0d][5:4]=%0b",i,my_array[i][5:4]);
+      end
+    $display("Using foreach loop----->");
+    foreach(my_array[i])
+      begin
+        $display("my_array[%0d][5:4]=%0b",i,my_array[i][5:4]);
+      end
+  end
+endmodule
