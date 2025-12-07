8d77d93d6f5dc9113a92bf46d9cabf9a43e98456
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-07 22:13:12 +0530

    Create 02.Use of $cast or casting.sv

---
+++ DAY11/Dyanmic casting/02.Use of $cast or casting.sv
@@ -0,0 +1,31 @@
+class parent_class;
+  bit [31:0] addr;
+
+  function display();
+    $display("Addr = %0d",addr);
+  endfunction
+endclass
+
+class child_class extends parent_class;
+  bit [31:0] data;
+
+  function display();
+    super.display();
+    $display("Data = %0d",data);
+  endfunction
+endclass
+
+module inheritence;
+  initial begin
+    parent_class p;
+    child_class  c=new();
+    child_class  c1;
+    c.addr = 10;
+    c.data = 20;
+
+    p = c;        
+    $cast(c1,p);   
+
+    c1.display();
+  end
+endmodule
