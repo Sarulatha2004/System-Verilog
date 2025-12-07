0fd00854ba95951411b47da9d17c0e3aecaf051b
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-07 16:02:19 +0530

    Create 01.Overriding the class.sv

---
+++ DAY11/01.Overriding the class.sv
@@ -0,0 +1,23 @@
+class parent_class;
+  bit [31:0]addr;
+  
+  function display();
+    $display("Addr=%0d",addr);
+  endfunction
+endclass
+
+class child_class extends parent_class;
+  bit [31:0] data;
+  function display();
+    $display("Data = %0d",data);
+  endfunction
+endclass
+
+module inheritence;
+  initial begin
+    child_class c=new();
+    c.addr = 10;
+    c.data = 20;
+    c.display();
+  end
+endmodule
