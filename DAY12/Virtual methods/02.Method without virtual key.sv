79ca19b7ce2640262851108c7992573d54d2e186
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-08 16:03:36 +0530

    Create 02.Method without virtual key.sv

---
+++ DAY12/Virtual methods/02.Method without virtual key.sv
@@ -0,0 +1,23 @@
+class base_class;
+  function void display();
+    $display("Inside the base class");
+  endfunction
+endclass
+
+class extended_class extends base_class;
+  function void display();
+    $display("Inside the extended class");
+  endfunction
+endclass
+
+module virtual_method;
+  base_class b_c;
+  extended_class e_c;
+  initial begin
+    e_c=new();
+    b_c=e_c;
+    b_c.display();
+  end
+endmodule
+
+//# KERNEL: Inside the base class
