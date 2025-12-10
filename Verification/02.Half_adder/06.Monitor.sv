7ce72887ea5b07d4b4e57414d8921c7a2139e4ae
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-10 21:58:08 +0530

    Create 06.Monitor.sv

---
+++ Verification/02.Half_adder/06.Monitor.sv
@@ -0,0 +1,26 @@
+class monitor;
+  virtual interf inf;
+  transaction trans;
+  mailbox montosco;
+  
+  function new(virtual interf inf, mailbox montosco);
+    this.inf=inf;
+    this.montosco=montosco;
+    
+  endfunction
+  
+  task main();
+    repeat(6) begin
+      #1;
+      trans=new();
+      trans.a=inf.a;
+      trans.b=inf.b;
+      trans.sum=inf.sum;
+      trans.carry=inf.carry;
+      
+      montosco.put(trans);
+      trans.display("Monitor");
+      #2;
+      end
+  endtask
+endclass
