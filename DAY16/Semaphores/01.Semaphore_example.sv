e81c698fc0a8207d9be7c7f51c29da201de2828d
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-15 15:49:14 +0530

    Update 01.Semaphore_example.sv

--- DAY16/Semaphores/01.Semaphore_example.sv
+++ DAY16/Semaphores/01.Semaphore_example.sv
@@ -20,4 +20,12 @@ initial begin
   $display("Thread 2:Done using 4 keys At time=[%0t]",$time);
 end
 endmodule
+
+////////////////output/////////////////////////////////////
+# KERNEL: In first initial block At time=[0]
+# KERNEL: Thread 1:Accessing 4 keys from semaphore At time=[0]
+# KERNEL: In second initial block At time=[0]
+# KERNEL: Thread 2:Accessing 4 keys from semaphore At time=[0]
+# KERNEL: Thresd 1: Done using 4 keys At time=[5]
+  # KERNEL: Thread 2:Done using 4 keys At time=[10]
   
