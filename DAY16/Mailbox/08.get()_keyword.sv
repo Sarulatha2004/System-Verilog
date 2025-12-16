8fb2860fdd6ee1ee67671d98032512aba927cbd0
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-16 21:33:25 +0530

    Create 08.get()_keyword.sv

---
+++ DAY16/Mailbox/08.get()_keyword.sv
@@ -0,0 +1,75 @@
+class A;
+  int a;
+  int i;
+  mailbox m;
+  
+  function new(mailbox m1);
+    this.m=m1;
+  endfunction
+  
+  task tra_data();
+    for(i=0;i<2;i++)
+      begin
+          a++;
+          m.put(a);
+          $display("[%0t] 1.Transmitter:value of a=%0d",$time,a);
+        end
+    $display("put successful");
+  endtask
+endclass
+
+class B;
+  
+  int a;
+  int i;
+  mailbox m;
+  
+  function new(mailbox m2);
+    this.m=m2;
+  endfunction
+  
+  task rec_data();
+    for(i=0;i<3;i++)
+      begin
+      m.get(a);
+      $display("[%0t] 2.Receiver: value of a=%0d",$time,a);
+    end
+    
+    $display("get successful");
+  endtask
+endclass
+
+module tb;
+  A a1;
+  B b1;
+  mailbox mb= new(2);
+  
+  initial begin
+    a1=new(mb);
+    b1=new(mb);
+    
+    repeat(2) begin
+      a1.tra_data();
+      $display("-------------------------------");
+      b1.rec_data();
+    end
+  end
+endmodule
+
+
+
+////////////////////////output////////////////////////////
+# KERNEL: [0] 1.Transmitter:value of a=1
+# KERNEL: [0] 1.Transmitter:value of a=2
+# KERNEL: put successful
+# KERNEL: -------------------------------
+# KERNEL: [0] 2.Receiver: value of a=1
+# KERNEL: [0] 2.Receiver: value of a=2
+
+/////////////////////////////////////
+
+
+
+
+
+
