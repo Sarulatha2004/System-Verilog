f7eeff138139e8bc0d197b49d64bc737193d1ebd
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2025-12-29 09:57:27 +0530

    Update 05.std::randomize inside the class.sv

--- DAY 17/Randomization/05.std::randomize inside the class.sv
+++ DAY 17/Randomization/05.std::randomize inside the class.sv
@@ -1,5 +1,5 @@
 class std_random_class;
-  int a;
+  rand int a;
   rand bit [2:0]b;
   constraint c1 { a>=2; a<=9;}
   
@@ -16,7 +16,7 @@ class std_random_class;
     if(std::randomize(c) with {c>1;c<4;});
     $display("using std_randomize with constraint ,c:%0d",c);
     void'(this.randomize());
-    $display("this randomize %0d %0d %0d",a,b,c);
+    $display("this randomize  %0d %0d %0d",a,b,c);
   endfunction
 endclass
 
@@ -28,3 +28,29 @@ module tb;
       t.display();
   end
 endmodule
+
+/////////////////////////output/////////////////////////////////////
+
+
+# KERNEL: Using std_randomize 1497363586,5
+# KERNEL: randomize(a) 6
+# KERNEL: using std_randomize(a) 484274802
+# KERNEL: using std_randomize b:5
+# KERNEL: using std_randomize with constraint ,c:2
+# KERNEL: this randomize  4 7 2
+# KERNEL: Using std_randomize -1621255588,0
+# KERNEL: randomize(a) 5
+# KERNEL: using std_randomize(a) -1608882576
+# KERNEL: using std_randomize b:2
+# KERNEL: using std_randomize with constraint ,c:3
+# KERNEL: this randomize  9 0 3
+# KERNEL: Using std_randomize 1182761674,6
+# KERNEL: randomize(a) 3
+# KERNEL: using std_randomize(a) -1520610950
+# KERNEL: using std_randomize b:3
+# KERNEL: using std_randomize with constraint ,c:3
+# KERNEL: this randomize  9 5 3
+
+
+
+//////////////////////////////////////////////////////////////////////////////
