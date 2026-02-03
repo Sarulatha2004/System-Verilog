a2f8e3f9f5b7fb4870dd2fded82add3efafbbace
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-03 13:37:30 +0530

    Create 04.Generator.sv

---
+++ Verification/04.Asynchronous up counter/04.Generator.sv
@@ -0,0 +1,24 @@
+class generator;
+  transaction trans;
+  mailbox gentodri;
+  
+
+  
+  function new(mailbox gentodri);
+    this.gentodri=gentodri;
+  
+  endfunction
+  
+  task main();
+    repeat(10)begin
+        trans =new();
+      assert(trans.randomize());
+
+      trans.display("Generator");
+      gentodri.put(trans);
+          #3;
+    end
+      
+     
+  endtask
+endclass
