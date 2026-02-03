e8246adc1ed9fe2ef7aac78134b859a839cd2120
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-03 11:49:57 +0530

    Create 04.Generator.sv

---
+++ Verification/03.D_Flipflop/04.Generator.sv
@@ -0,0 +1,23 @@
+class generator;
+  transaction trans;
+  mailbox gentodri;
+  
+
+  
+  function new(mailbox gentodri);
+    this.gentodri=gentodri;
+  endfunction
+  
+  task main();
+    repeat(5)begin
+        trans =new();
+      assert(trans.randomize());
+
+      trans.display("Generator");
+      gentodri.put(trans);
+          #3;
+    end
+      
+     
+  endtask
+endclass
