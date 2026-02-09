048e93b8059c2cc26ff81b12338eb75dcc12b891
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-09 12:42:02 +0530

    Create 03.Transaction.sv

---
+++ Verification/06.Synchronous_FIFO_verification/03.Transaction.sv
@@ -0,0 +1,18 @@
+class transaction#(parameter DATA_WIDTH=8);
+  
+  bit clk;
+  bit rstn;
+  rand bit wr_en;
+  rand bit rd_en;
+  rand bit [DATA_WIDTH-1:0]data_in;
+  bit [DATA_WIDTH-1:0]data_out;
+  bit full,empty;
+  
+  constraint cons{ wr_en ^ rd_en; }
+  
+  function void display(string name);
+    
+    $display("-------------%s---------------",name);
+    
+  endfunction
+endclass
