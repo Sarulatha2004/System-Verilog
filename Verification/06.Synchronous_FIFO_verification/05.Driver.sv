64677a7212bb0b1a7e6572f7406a12a54f407466
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-02-09 12:44:22 +0530

    Create 05.Driver.sv

---
+++ Verification/06.Synchronous_FIFO_verification/05.Driver.sv
@@ -0,0 +1,19 @@
+class generator;
+  transaction trans;
+  mailbox gentodri;
+  
+  function new (mailbox gentodri);
+    this.gentodri=gentodri;
+  endfunction
+  
+  task main();
+    
+    repeat(50) begin
+      trans=new();
+      
+      assert(trans.randomize());
+      gentodri.put(trans);
+      #3;
+    end
+  endtask
+endclass
